556ca5d05809059cd3589c5877fa879a6c71c0a9
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-03-24 12:57:46 +0530

    App and Index file setup

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+//create an instance of express server
+const app = express();
+
+//Just mount the middlewares here
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: '16kb'
+}));
+
+app.use(express.urlencoded({
+    limit: '16kb',
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+export default app;
