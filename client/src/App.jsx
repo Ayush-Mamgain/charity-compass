1612327ef99d338931c081f543e0645cf664bd87
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-03-24 23:49:17 +0530

    User registration is now complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import RegisterUser from "./pages/register/RegisterUser";
 import { Routes, Route } from "react-router-dom";
 import LoginUser from "./pages/login/LoginUser.jsx";
+import Home from "./pages/home/Home.jsx";
+// import { Toaster } from "react-hot-toast";
 
 function App() {
     return (
             <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
+                <Route exact path="/" element={<Home />} />
                 <Route exact path="/loginUser" element={<LoginUser />} />
                 <Route exact path="/registerUser" element={<RegisterUser />} />
             </Routes>
--- client/src/pages/register/RegisterUser.jsx
+++ client/src/pages/register/RegisterUser.jsx
@@ -4,13 +4,18 @@ import loginBg from "./login-bg.png";
 import { NavLink } from "react-router-dom";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function RegisterUser() {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({
         username: "",
         email: "",
-        createPassword: '',
-        confirmPassword: ''
+        password: "",
+        confirmPassword: "",
     });
 
     const [viewPassword, setViewPassword] = useState(false);
@@ -28,9 +33,30 @@ function RegisterUser() {
         });
     }
 
-    function submitHandler(event) {
+    async function submitHandler(event) {
         //make a post request to register user
         event.preventDefault();
+        try {
+            const response = await fetch(`${API_URL}/api/users/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            }); //no need to use catch block here, response will always be returned
+            const result = await response.json();
+            console.log(result);
+            if (result.success) {
+                toast.success(result.message);
+                //Sign in the user (update global state)
+                setTimeout(() => navigate('/loginUser'), 1000);
+            } else {
+                toast.error(result.message);
+            }
+        } catch(error) {
+            console.error(error);
+            //navigate to the default error page
+        }
     }
 
     return (
@@ -39,10 +65,10 @@ function RegisterUser() {
             <form onSubmit={submitHandler} className="login__form">
                 <h1 className="login__title">Register</h1>
                 <div className="login__inputs">
-                <div className="login__box">
+                    <div className="login__box">
                         <input
-                            name="text"
-                            type="username"
+                            type="text"
+                            name="username"
                             placeholder="Username"
                             required
                             className="login__input"
@@ -61,7 +87,7 @@ function RegisterUser() {
                     </div>
                     <div className="login__box">
                         <input
-                            name="createPassword"
+                            name="password"
                             type="password"
                             placeholder="Create Password"
                             required
@@ -97,13 +123,14 @@ function RegisterUser() {
                     className="login__button"
                     onClick={submitHandler}
                 >
-                    Login
+                    Register
                 </button>
                 <div className="login__register">
                     Don't have an account?{" "}
                     <NavLink to="/loginUser">Login</NavLink>
                 </div>
             </form>
+            <Toaster position="top-center" />
         </div>
     );
 }
