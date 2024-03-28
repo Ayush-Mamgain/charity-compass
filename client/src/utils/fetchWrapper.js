045f2cb0b45528acff5742db946de00fe798cb5e
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-03-28 15:44:43 +0530

    Included promised based fetch wrapping

--- client/src/utils/fetchWrapper.js
+++ client/src/utils/fetchWrapper.js
@@ -1,29 +1,44 @@
-import { useNavigate } from "react-router-dom";
 
-//this function is for future use
-async function fetchWrapper(requestUrl, requestOptions) {
+
+async function getNewAccessToken() {
     const API_URL = import.meta.env.VITE_API_URL;
     const refreshTokenUrl = `${API_URL}/api/refreshToken`;
-    const navigate = useNavigate();
 
-    async function getNewAccessToken() {
-        try {
-            const response = await fetch(refreshTokenUrl); //this would store the token in the cookie
-            const result = await response.json();
-            return result.data.accessToken;
-        } catch(error) {
-            //unable to get new access token --> need to login again
-            console.error(error);
-            console.log('Session expired, Please login again');
-            navigate('/loginUser');
-        }
-    }
+    const response = await fetch(refreshTokenUrl); //this would store the token in the cookie
+    if (response.status !== 200) //session expired
+        return null;
+    const result = await response.json();
+    return result.data.accessToken;
+}
 
+async function fetchWrapper(requestUrl, requestOptions) {
     try {
         let response = await fetch(requestUrl, requestOptions)
-        if(result.statusCode === 401) { //Unauthorized access
+        if (response.status === 401) { //Unauthorized access
             //access token expired --> request for new access token
-            const newAccessToken = getNewAccessToken();
+            const retryOptions = {
+                ...requestOptions,
+                headers: {
+                    ...requestOptions.headers,
+                    Authorization: `Bearer ${await getNewAccessToken()}`
+                }
+            }
+            response = await fetch(requestUrl, retryOptions);
+        }
+        return response;
+    } catch (error) {
+        console.error('Error in fetching with refresh toekn:\n', error);
+        throw error;
+    }
+}
+
+function fetchPromiseWrapper(requestUrl, requestOptions = {}) {
+    return new Promise(async (resolve, reject) => {
+        let response = await fetch(requestUrl, requestOptions);
+        if (response.status === 401) {
+            const newAccessToken = await getNewAccessToken();
+            if (!newAccessToken)
+                reject(new Error('Session expired, login again'));
             const retryOptions = {
                 ...requestOptions,
                 headers: {
@@ -31,13 +46,11 @@ async function fetchWrapper(requestUrl, requestOptions) {
                     Authorization: `Bearer ${newAccessToken}`
                 }
             }
+            //make the API call again
             response = await fetch(requestUrl, retryOptions);
         }
-        return response;
-    } catch(error) {
-        console.error('Error in fetching with refresh toekn:\n',error);
-        throw error;
-    }
+        resolve(response);
+    });
 }
 
-module.exports = fetchWrapper;
+module.exports = { fetchWrapper, fetchPromiseWrapper };
